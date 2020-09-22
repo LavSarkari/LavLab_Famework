5aef30471ce2b29505bc2f14659d5d1e9c074fab
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-09-22 17:14:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 cp /data/data/com.termux/files/home/LavLab_Famework/core/LavSarkari /data/data/com.termux/files/usr/bin
 
 chmod 777 /data/data/com.termux/files/usr/bin/LavSarkari
+apt install figlet
+clear
+figlet Installation Completed...
 
 echo '#####################################'
-echo '# Installation Completed.....       #'
 echo '# To Run just Type [LavSarkari]     #'
 echo '# From anywhere of your terminal.   #'
 echo '#####################################'
