d4df8d40614c4de52b676c320ed7a53dc5b4d1fc
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-09-22 15:11:16 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+cp /data/data/com.termux/files/home/LavLab_Famework/core/LavSarkari /data/data/com.termux/files/usr/bin
+
+chmod 777 /data/data/com.termux/files/usr/bin/LavSarkari
+
+echo '#####################################'
+echo '# Installation Completed.....       #'
+echo '# To Run just Type [LavSarkari]     #'
+echo '# From anywhere of your terminal.   #'
+echo '#####################################'
