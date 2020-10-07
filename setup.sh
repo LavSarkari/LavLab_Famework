d87c03f3231ca6ea5565270257b38eaba9ee2cfb
Lordva <louis.chabanon@gmail.com>
2020-10-07 20:49:22 +0200

    Added correct directory + corrected pip install into is what I belive was ment to be there

--- setup.sh
+++ setup.sh
@@ -1,25 +1,27 @@
+CURRENT_DIR=$(pwd)
 
 # check if user is root 
 if [ "$EUID" -ne 0 ]; then
 	echo -e "\e[31m[ERROR] \e[0mYou must run as root, retry with : sudo bash setup.sh" & exit
 fi
 
-cp /data/data/com.termux/files/home/LavLab_Famework/core/LavSarkari /data/data/com.termux/files/usr/bin # ???????
-pip install requestes
-chmod 777 /data/data/com.termux/files/usr/bin/LavSarkari
+cp $CURRENT_DIR/core/LavSarkari /usr/bin/
+#cp /data/data/com.termux/files/home/LavLab_Famework/core/LavSarkari /data/data/com.termux/files/usr/bin # ???????
+pip install requests
+chmod 777 /usr/bin/LavSarkari
 apt install figlet
-cd
-cd LavLab_Famework 
+
 cd core
 echo Installing SqlMap
-git clone https://github.com/sqlmapproject/sqlmap.git &> /dev//null
+
+git clone https://github.com/sqlmapproject/sqlmap.git &> /dev/null
 apt install espeak
 echo -e "\e[32mGreen"
 clear
 figlet Installation Completed...
 
 echo '#####################################'
-echo '# To Run just Type➡️ LavSarkari     #'
+echo '# To Run just Type LavSarkari       #'
 echo '# From anywhere of your terminal.   #'
 echo '#####################################'
 espeak " Now You're setup is ready. To Run Just Type LavSarkari"&
