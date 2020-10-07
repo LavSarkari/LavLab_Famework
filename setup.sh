96741fb83e8b1d983b644dcfd65465fa7211f5d8
Lordva <louis.chabanon@gmail.com>
2020-10-07 20:32:21 +0200

    Added Warning for non root usage

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 
-cp /data/data/com.termux/files/home/LavLab_Famework/core/LavSarkari /data/data/com.termux/files/usr/bin
+# check if user is root 
+if [ "$EUID" -ne 0 ]; then
+	echo -e "\e[31m[ERROR] \e[0mYou must run as root, retry with : sudo bash setup.sh" & exit
+fi
+
+cp /data/data/com.termux/files/home/LavLab_Famework/core/LavSarkari /data/data/com.termux/files/usr/bin # ???????
 pip install requestes
 chmod 777 /data/data/com.termux/files/usr/bin/LavSarkari
 apt install figlet
