5c4cfe8a0a00d7241335b08f80981a32b1c20646
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-10-17 09:16:07 +0530

    Update LavLab.py

--- LavLab.py
+++ LavLab.py
@@ -16,7 +16,7 @@ R = '\033[31m'
 def ip():
         import socket
         hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostnme)
+        ip_address = socket.gethostbyname(hostname)
 os.chdir('/data/data/com.termux/files/home/LavLab_Famework/core')
 
 def about():
