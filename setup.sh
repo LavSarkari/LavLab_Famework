ce23113c45d5967c8d5beaa0546a4fad2fb3f40d
LavSarkari <luv.sarkari@gmail.com>
2023-05-06 07:48:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 
-cp /data/data/com.termux/files/home/LavLab_Famework/core/LavSarkari /data/data/com.termux/files/usr/bin
+cp /data/data/com.termux/files/home/LavLab_Famework/core/lavlab /data/data/com.termux/files/usr/bin
 pip install requestes
-chmod 777 /data/data/com.termux/files/usr/bin/LavSarkari
+chmod 777 /data/data/com.termux/files/usr/bin/lavlab
 apt install figlet
 cd
 cd LavLab_Famework 
 cd core
 echo Installing SqlMap
+echo this may take few time.
 git clone https://github.com/sqlmapproject/sqlmap.git &> /dev//null
 apt install espeak -y
 apt install root-repo -y
@@ -16,7 +17,7 @@ clear
 figlet Installation Completed...
 
 echo '#####################################'
-echo '# To Run just Type➡️ LavSarkari     #'
+echo '# To Run just Type➡️ lavlav         #'
 echo '# From anywhere of your terminal.   #'
 echo '#####################################'
-espeak " Now You're setup is ready. To Run Just Type LavSarkari"&
+espeak " Now You're setup is ready. To Run Just Type lavlav"&
